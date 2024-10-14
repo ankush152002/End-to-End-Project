8ca709a35b256202d7daf0f579267650fb3753f5
Ankush Singh <98583838+ankush152002@users.noreply.github.com>
2024-10-14 14:45:44 +0530

    Update app.js

--- node-apps/node-hello-1/app.js
+++ node-apps/node-hello-1/app.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js App X!');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
 
