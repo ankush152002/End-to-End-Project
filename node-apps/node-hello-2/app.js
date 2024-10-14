bf3b095a3bd293f42901910fa871c7506f9da948
Ankush Singh <98583838+ankush152002@users.noreply.github.com>
2024-10-14 14:58:12 +0530

    added docker file

--- node-apps/node-hello-2/app.js
+++ node-apps/node-hello-2/app.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 80;
+
+app.get('/', (req, res) => {
+    res.send('Hello from Application 1!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
